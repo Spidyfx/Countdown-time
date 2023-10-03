6903d0d8d6c387b9d591d49b56d2221e9988e79e
Malesela Simon Malapane <65837866+Simon339@users.noreply.github.com>
2023-10-04 01:39:05 +0200

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const lunchDay = new Date(`October 3 ${currentYear } 19:50:00 GMT+02:00`);
+const lunchDay = new Date(`January 1 ${currentYear + 1} 00:00:00 GMT+02:00`);
 
 //update Countdowntime
 function updateCountdown() {
