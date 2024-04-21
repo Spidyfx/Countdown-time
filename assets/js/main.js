95449cf0fbddab2b9475228f5a46e8da36d1718d
Spidy <suthishgamer2@gmail.com>
2024-04-21 17:01:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const lunchDay = new Date(`January 1 ${currentYear + 1} 00:00:00 GMT+02:00`);
+const lunchDay = new Date(`May 5 2024 00:00:00 GMT+05:30`);
 
 //update Countdowntime
 function updateCountdown() {
